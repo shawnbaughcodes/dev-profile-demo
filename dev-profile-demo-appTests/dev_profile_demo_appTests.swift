39d37a199d4b7a50a53447827a9ffd61ff060ce9
Shawn Baugh II <shawnbaughcodes@gmail.com>
2019-04-19 20:31:52 -0500

    Initial Commit

---
+++ dev-profile-demo-appTests/dev_profile_demo_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  dev_profile_demo_appTests.swift
+//  dev-profile-demo-appTests
+//
+//  Created by Shawn Baugh on 4/19/19.
+//  Copyright © 2019 Shawn Baugh. All rights reserved.
+//
+
+import XCTest
+@testable import dev_profile_demo_app
+
+class dev_profile_demo_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
